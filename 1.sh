ad04385254ab4f7b8e4c036503cb7cf58c6f3928
梁朝阳 <budongjishubu@gmail.com>
2026-04-13 14:04:31 +0800

    删

--- 1.sh
+++
@@ -1,7 +0,0 @@
-echo "# 2025-TMC" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:Zhaoyang-Liang/2025-TMC.git
-git push -u origin main
